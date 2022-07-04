614425224c1bd1cf96f215938dc6f40af140f671
laksh <lakshminarasimhan254@gmail.com>
2022-07-03 22:24:50 -0400

    Converted to Maven Project

---
+++ JavaConcepts/src/com/mln/dbconnection/SelectQuery.java
@@ -0,0 +1,40 @@
+/*********************
+ **Date:{date}*******
+ *********************
+ *********************/
+package com.mln.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ ********************
+ * @author laksh
+ ********************
+ **/
+public class SelectQuery {
+
+	//This Method is created to 
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://root@localhost/employees");
+		Statement statement = connection.createStatement();
+		statement.execute("SELECT * FROM `employees` WHERE `emp_no` IN (10000,10002,10004)");
+		ResultSet rs = statement.getResultSet();
+		
+		while(rs.next()){
+			System.out.println(rs.getString("first_name"));
+			
+		}
+		rs.close();
+		statement.close();
+		connection.close();
+	}
+
+}
